e8babc507588e01a114e52de45fae12b70bf43fe
Advirao <advirao@gmail.com>
2025-04-10 20:46:22 -0500

    adding 2 new python files

---
+++ variable.py
@@ -0,0 +1,16 @@
+# This is comment
+
+#Integer
+launch_count = 10
+#Float
+success_rate = 0.95
+#String
+rocket_name = 'Falcon 9'
+#Boolean
+is_active = True
+
+# Basic operation
+
+Total_launches = launch_count + 5
+
+print(Total_launches)
